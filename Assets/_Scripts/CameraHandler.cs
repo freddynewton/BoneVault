7f9ca7f421273289655c0858c978d406b5936226
Freddy Newton <ghostlopment@gmail.com>
2020-09-07 22:14:17 +0200

    player movement & basic Scene

---
+++ Assets/_Scripts/CameraHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public PlayerController player;
+
+    // private values
+    private float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookAtMouse();
+    }
+
+    private void lookAtMouse()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * player.unit.stats.mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * player.unit.stats.mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector] public PlayerUnit unit;
+    [HideInInspector] public CharacterController controller;
+
+
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        unit = GetComponent<PlayerUnit>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        movement();
+    }
+
+    private void movement()
+    {
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        Vector3 groundVec = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - gameObject.transform.localScale.y, gameObject.transform.position.z);
+
+        bool isGrounded = Physics.CheckSphere(groundVec, unit.stats.groundDistance, unit.stats.groundMask);
+
+        controller.Move(move * unit.stats.movementSpeed * Time.deltaTime);
+
+        Debug.Log(isGrounded);
+        
+
+        // Ground Check
+        if (isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(unit.stats.jumpHeight * -2f * unit.stats.gravity);
+        }
+
+        velocity.y += unit.stats.gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - gameObject.transform.localScale.y, gameObject.transform.position.z), 0.4f);
+    }
+}

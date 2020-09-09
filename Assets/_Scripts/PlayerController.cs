0b02fcd5ca8e095249073cd070c5c1a6a21fa1a7
Pixelpato <pixelpato@gmail.com>
2020-09-09 14:04:50 +0200

    added sprint

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,13 +9,17 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] public PlayerUnit unit;
     [HideInInspector] public CharacterController controller;
 
-
     private Vector3 velocity;
+    private float baseSpeed;
+    private float sprintSpeed;
+
 
     private void Start()
     {
         unit = GetComponent<PlayerUnit>();
         controller = GetComponent<CharacterController>();
+        baseSpeed = unit.stats.movementSpeed;
+        sprintSpeed = unit.stats.movementSpeed * 2f;
     }
 
     private void Update()
@@ -28,7 +32,7 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
         Vector3 groundVec = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - gameObject.transform.localScale.y, gameObject.transform.position.z);
 
-        bool isGrounded = Physics.CheckSphere(groundVec, unit.stats.groundDistance, unit.stats.groundMask);
+        bool isGrounded = Physics.CheckSphere(groundVec, unit.stats.groundDistance, unit.stats.groundMask);      
 
         controller.Move(move * unit.stats.movementSpeed * Time.deltaTime);
 
@@ -43,6 +47,13 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(unit.stats.jumpHeight * -2f * unit.stats.gravity);
+        }
+
+        if (Input.GetButton("Sprint")) {
+            unit.stats.movementSpeed = sprintSpeed;
+        }
+        else {
+            unit.stats.movementSpeed = baseSpeed;
         }
 
         velocity.y += unit.stats.gravity * Time.deltaTime;

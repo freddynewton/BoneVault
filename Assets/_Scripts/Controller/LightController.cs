f48aa74ca8e1dd1460cab062e1855f8efa581ad2
Pixelpato <pixelpato@gmail.com>
2020-09-14 12:50:04 +0200

    added light controller, unified texture sizes, tweaked hit animation

---
+++ Assets/_Scripts/Controller/LightController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public AnimationCurve animationCurve;
+    private float animationTime;
+    private Light torchLight;
+    private float timeElapsed;
+
+
+    void Start()
+    {
+        torchLight = transform.GetComponent<Light>();
+        timeElapsed = 0;
+        animationTime = animationCurve[animationCurve.length - 1].time;
+    }
+
+    void FixedUpdate()
+    {
+        if (timeElapsed < animationTime) {
+            timeElapsed += Time.deltaTime;
+        }
+        else timeElapsed = 0;
+
+        torchLight.intensity = animationCurve.Evaluate(timeElapsed);
+    }
+}

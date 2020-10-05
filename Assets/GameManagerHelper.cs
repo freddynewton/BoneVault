50b270c9649358f3fccd7f7bcdad4f0b7854fb6d
Fred Newton <ghostlopment@gmail.com>
2020-10-05 10:59:21 +0200

    Game Manager and Performance Log saver

---
+++ Assets/GameManagerHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerHelper : MonoBehaviour
+{
+    public static GameManagerHelper Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // Disables all Logs in Builds to save performance
+#if UNITY_EDITOR
+        Debug.unityLogger.logEnabled = true;
+#else
+        Debug.unityLogger.logEnabled = false; 
+#endif
+    }
+
+
+}

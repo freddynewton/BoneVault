58cb220504327fe579dec68dea125d23712d67e0
Freddy Newton <ghostlopment@gmail.com>
2020-10-11 00:30:32 +0200

    room start

---
+++ Assets/_Scripts/PCG/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public enum RoomType
+    {
+        START_ROOM,
+        ENEMIE_ROOM,
+        BOSS_ROOM,
+        EXIT_ROOM
+    }
+
+    [Header("Basic Room Settings")]
+    public RoomType roomType;
+    public List<Door> doors;
+    
+    public void setDoors(bool isOpen)
+    {
+        foreach(Door d in doors)
+        {
+            if (isOpen) d.openDoor();
+            else d.closeDoor();
+        }
+    }
+}

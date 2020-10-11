a328e58e8b578ccde4b12bda7de88d09329f160f
Freddy Newton <ghostlopment@gmail.com>
2020-10-11 14:03:21 +0200

    Door Function

--- Assets/_Scripts/PCG/Door.cs
+++ Assets/_Scripts/PCG/Door.cs
@@ -6,11 +6,11 @@ public class Door : MonoBehaviour
 {
     public void openDoor()
     {
-
+        LeanTween.moveLocalY(gameObject, 6, 2).setEaseOutBounce();
     }
 
     public void closeDoor()
     {
-
+        LeanTween.moveLocalY(gameObject, 0, 2).setEaseOutBounce();
     }
 }

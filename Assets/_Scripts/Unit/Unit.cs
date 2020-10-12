7413f6999d3257a3cd1cc7406e7def623ac038cf
Freddy Newton <ghostlopment@gmail.com>
2020-10-12 12:48:23 +0200

    knockback

--- Assets/_Scripts/Unit/Unit.cs
+++ Assets/_Scripts/Unit/Unit.cs
@@ -94,7 +94,16 @@ public class Unit : MonoBehaviour
         Time.timeScale = 1;
     }
 
-    private void knockback(Vector3 otherPos, float kb) => rb.AddForce((gameObject.transform.position - otherPos).normalized * kb, ForceMode.Impulse);
+    private void knockback(Vector3 otherPos, float kb)
+    {
+
+        if (gameObject.CompareTag("Enemy"))
+        {
+            LeanTween.move(gameObject, (gameObject.transform.position - otherPos) * kb, 1).setEaseOutExpo();
+        }
+
+        // rb.AddForce((gameObject.transform.position - otherPos).normalized * kb, ForceMode.Impulse);
+    }
 
     public IEnumerator flashWhite(float time)
     {

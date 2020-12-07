db668ba705085390ac222e76cfbf5d99e51ea448
Pixelpato <pixelpato@gmail.com>
2020-12-07 13:38:58 +0100

    vase sfx implement, trapdoor implement

--- Assets/_Scripts/Interactables/DestroyableEntity.cs
+++ Assets/_Scripts/Interactables/DestroyableEntity.cs
@@ -4,17 +4,29 @@ using System.Collections;
 
 public class DestroyableEntity : Interactable
 {
+    public AudioClip [] destroySFX;
     private ParticleSystem vfx;
 
     public override void interact()
     {
         vfx.Play();
         GFX.SetActive(false);
+        playSFX(destroySFX, GetComponent<AudioSource>(), true);
     }
 
 
     private void Start()
     {
         vfx = gameObject.GetComponentInChildren<ParticleSystem>();
+    }
+
+    // SFX Handler
+    public void playSFX (AudioClip [] sounds, AudioSource source, bool random) {
+        if (random) source.clip = sounds [Random.Range(0, sounds.Length)];
+        else source.clip = sounds [0];
+
+        source.pitch = Random.Range(0.8f, 1.2f);
+
+        if (source != null) source.Play();
     }
 }

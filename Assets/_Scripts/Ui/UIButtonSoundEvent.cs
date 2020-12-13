0afc50e2b1773144fdf463257bb11083467a9182
Pixelpato <pixelpato@gmail.com>
2020-12-13 14:31:18 +0100

    menu sfx

---
+++ Assets/_Scripts/Ui/UIButtonSoundEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class UIButtonSoundEvent : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler {
+    public AudioClip hoverSFX;
+    public AudioClip clickSFX;
+
+
+    public void OnPointerEnter (PointerEventData ped) {
+        SoundManager.Instance.playSFX(hoverSFX, GetComponent<AudioSource>(), 1, 1);
+    }
+
+    public void OnPointerDown (PointerEventData ped) {
+        SoundManager.Instance.playSFX(clickSFX, GetComponent<AudioSource>(), 1, 1);
+    }
+}
